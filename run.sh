16c6cf47c96fb885023122d0cf9331ceeecd5346
Pablo Emanuell Lopes Targino <pabloemanuell@A204-06L.imd.local>
2019-03-01 16:57:37 -0300

    up

---
+++ run.sh
@@ -0,0 +1,9 @@
+mkdir ~/tmp
+cd ~/tmp
+wget 'https://go.microsoft.com/fwlink/?LinkID=620884'
+mkdir ~/softwares
+cd ~/softwares
+tar -xvf *
+mkdir ~/bin
+cd ~
+ln -s ~/softwares/VS*/bin/code ~/bin/code
